e2a4c8ba63e4f5bec90e6f24f228545e809b334f
Luka Goes Ribeiro <luka.ribeiro@codevasf.gov.br>
2026-09-16 14:25:15 -0300

    mostrar se tem suco e o valor

--- sistema_vendas.py
+++ sistema_vendas.py
@@ -1,5 +1,10 @@
 frutas = ["maçã", "banana", "laranja", "uva", "abacaxi"]
 sucos = frutas
+valores = [8]
 while True:
-
-       
+    if input_frutas := input("Digite o nome de uma fruta: ").lower() in frutas:
+        print("Temos esse suco!")
+        print(f"O valor do suco de 400ml é R${valores[0]:.2f}!!")
+    else:
+        print("Não temos esse suco!")
+        break
